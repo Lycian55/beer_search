4b1669846c9c2570c8fc306e27f6bc886bc495a3
Denis Donevski <lycian@Deniss-MacBook-Pro.local>
2024-05-13 10:27:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import $ from "jquery";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.openbrewerydb.org/v1/breweries";
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    const breweryNames = result.data.map((brewery) => brewery.name);
+    const breweryCity = result.data.map((brewery) => brewery.city);
+    const breweryState = result.data.map((brewery) => brewery.state);
+    const breweryStreet = result.data.map((brewery) => brewery.street);
+    const breweryPhone = result.data.map((brewery) => brewery.phone);
+    res.render("index.ejs", {
+      breweryNames: breweryNames,
+      breweryCity: breweryCity,
+      breweryState: breweryState,
+      breweryStreet: breweryStreet,
+      breweryPhone: breweryPhone,
+    });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
